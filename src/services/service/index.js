6320be5db1a33e104c8213027b1ea00772b11a3d
wellesleymussolini <iei.muss@gmail.com>
2023-05-19 13:26:32 -0300

    Feature - services

--- src/services/service/index.js
+++ src/services/service/index.js
@@ -3,4 +3,7 @@ import { server } from "../axios/axios";
 export const employees = {
     get: async () => await server.get("/employees").then((response) => response.data).catch((error) => error),
     getById: async (id) => await server.get("/employees/" + id).then((response) => response.data).catch((error) => error),
+    post: async (employee) => await server.post("/employees", employee).then((response) => response.data).catch((error) => error),
+    put: async (id, employee) => await server.put("/employees/" + id, employee).then((response) => response.data).catch((error) => error),
+    delete: async (id) => await server.delete("/employees/" + id).then((response) => response.data).catch((error) => error),
 };
