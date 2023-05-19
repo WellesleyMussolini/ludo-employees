6ed00b87ed0a94a84829f5650bc9abe979d2fbdf
wellesleymussolini <iei.muss@gmail.com>
2023-05-19 13:22:49 -0300

    Feature - mock

---
+++ src/components/mock/week.mock.js
@@ -0,0 +1,30 @@
+export const week_days = [
+    {
+        week_day: "segunda",
+        border: true
+    },
+    {
+        week_day: "terça",
+        border: true
+    },
+    {
+        week_day: "quarta",
+        border: true
+    },
+    {
+        week_day: "quinta",
+        border: true
+    },
+    {
+        week_day: "sexta",
+        border: true
+    },
+    {
+        week_day: "sábado",
+        border: true
+    },
+    {
+        week_day: "domingo",
+        border: false
+    },
+];
