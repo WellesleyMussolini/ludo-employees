7f5d637ee680928035f733134c5af5d1282198ab
wellesleymussolini <iei.muss@gmail.com>
2023-05-22 20:48:52 -0300

    Feature - styles

---
+++ src/pages/home/components/week/week.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 70em;
+    height: 20em;
+    background: #fff;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+`;
