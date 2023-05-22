8a79c10ec61c4281122a886de78a89d3ee7b9a5a
wellesleymussolini <iei.muss@gmail.com>
2023-05-22 20:48:35 -0300

    Feature - home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,35 @@
 import React, { useState } from "react";
-import { Container, Wrapper } from "./home.styles";
+import { Container } from "./home.styles";
+import Week from "./components/week/Week.component";
+import Button from "../../components/button/Button";
+import Modal from "../../components/modal/work-days/Modal";
 
 const Home = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const [staffer, setStaffer] = useState("");
 
+    const handleSubmit = () => {
+        console.log(staffer)
+    };
     return (
         <Container>
-            <Wrapper>
-
-            </Wrapper>
-
-
-
+            <Button
+                text="ADICIONAR FUNCIONÁRIO"
+                handleOnClick={() => setOpenModal(true)}
+                backgroundColor="rgb(7, 204, 40)"
+                fontSize="8px"
+                width="230px"
+                height="40px"
+                position="fixed"
+            />
+            <Modal 
+                open={openModal}
+                close={() => setOpenModal(false)}
+                staffer={staffer}
+                HandleStaffer={setStaffer}
+                HandleSubmit={handleSubmit}
+            />
+            <Week />
         </Container>
     );
 };
--- src/pages/home/home.styles.js
+++ src/pages/home/home.styles.js
@@ -5,21 +5,6 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
-    height: 80vh;
+    min-height: calc(100vh - 80px);
+    background: #F4F4F4;
 `;
-
-export const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    top: 10em;
-    flex-direction: column;
-    margin-left: auto;
-    margin-right: auto;
-  
-    & > * {
-        margin-bottom: 5em; /* adiciona margem inferior para cada elemento filho */
-    }
-`;
-
