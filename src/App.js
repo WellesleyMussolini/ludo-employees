e2edaabd702aeb83a3764c452b11660ecf876220
wellesleymussolini <iei.muss@gmail.com>
2023-06-20 17:29:34 -0300

    refactoring the repository

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import Routers from "./routes/routes";
-
-const App = () => <Routers />;
-
-export default App;
--- src/services/axios/axios.js
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-
-export const server = axios.create({ baseURL: "http://localhost:3300" }); 
